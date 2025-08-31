42978734fe1a062a3ccf3b78bb4818023026136e
Nuselda <nana_gersh@mail.ru>
2025-08-31 11:06:02 +0300

    Update myshow_parser.py

--- myshow_parser.py
+++ myshow_parser.py
@@ -23,7 +23,7 @@ def collect_user_rates(user_login):
         show_rate = entry.find('div', class_='Rating Rating--disabled Rating--active Rating--size-s mobile tablet')
         if show_rate:
             rate1 = show_rate.find('div', class_='Rating__wrapper')
-            rate = rate1.get('title')
+            rate = rate1.get('title')[-1]
         else:
             rate = 0
 
@@ -35,3 +35,4 @@ user_rates = collect_user_rates(user_login='Nog')
 df = pd.DataFrame(user_rates)
 
 df.to_excel('user_rates.xlsx')
+
